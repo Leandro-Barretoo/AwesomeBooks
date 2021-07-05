31df0fa0a29400cc7680ea7df548626d81d80309
Luqman Musah <musahluqman@gmail.com>
2021-07-05 13:36:44 +0000

    Add display functionality

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,14 @@ function adBook(events) {
     }
     collection.push(book);
     document.querySelector('form').reset();
+
+    let listItem = document.createElement('p');
+    let itemInfo = document.createTextNode(book['title'] + "\n" + book['author']);
+    listItem.appendChild(itemInfo);
+    let container = document.getElementById('list');
+    container.appendChild(listItem);
 }
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('sub').addEventListener('click', adBook)
 })
+
