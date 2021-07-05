1f3a4dd1d65a159c06cbb1cac4c60183e08ec99a
Luqman Musah <musahluqman@gmail.com>
2021-07-05 13:12:14 +0000

    Add book function

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,14 @@
+let collection = []
 
-
-
-
+function adBook(events) {
+    events.preventDefault();
+    let book = {
+        title: document.getElementById('title').value,
+        author: document.getElementById('author').value
+    }
+    collection.push(book);
+    document.querySelector('form').reset();
+}
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('sub').addEventListener('click', adBook)
+})
