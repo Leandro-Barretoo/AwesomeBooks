d79965813301450c7d3e90bbbe72841a756ecffd
Luqman Musah <musahluqman@gmail.com>
2021-07-06 16:40:33 +0000

    Add class create, remove and display

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,44 @@
 let collection = JSON.parse(localStorage.getItem('myBookList')) || [];
 
+class createBook {
+  constructor(title, author) {
+    this.title = title
+    this.author = author
+  }
+  listInsert(value) {
+    collection.push(value);
+  }
+}
+class removeBtn {
+  static deleteBtn(listBtn, value) {
+    listBtn.addEventListener('click', (ev) => {
+      const button = ev.target;
+      button.parentNode.parentNode.removeChild(button.parentNode);
+      this.outCollection(value);
+    })
+  }
+  static outCollection(value) {
+    const itemIndex = collection.indexOf(value);
+    collection.splice(itemIndex, 1);
+    saveLocal.saveList();
+  }
+}
+class displayList {
+  static create(value) {
+    const mainList = document.getElementById('main-list');
+    const itemContainer = document.createElement('div');
+    mainList.appendChild(itemContainer);
+    const listItem = document.createElement('p');
+    const itemInfo = document.createTextNode(`Title: ${value.title} | Author: ${value.author}`);
+    listItem.appendChild(itemInfo);
+    itemContainer.appendChild(listItem);
+    const listBtn = document.createElement('button');
+    listBtn.innerText = 'Remove';
+    itemContainer.appendChild(listBtn);
+    removeBtn.deleteBtn(listBtn, value);
+  }
+}
+
 class preserve {
   static individualElems() {
     for(let i = 0; i < collection.length; i++) {
@@ -17,3 +56,4 @@ document.addEventListener('DOMContentLoaded', () => {
   preserve.initialBook();
   preserve.individualElems();
 })
+
