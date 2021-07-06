9e350fccd770e392a24250a6a89eadaac4021295
Leandro <officialdevmi@gmail.com>
2021-07-06 09:33:00 -0700

    Add preseve functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,47 +1,19 @@
-/* eslint-disable indent */
+let collection = JSON.parse(localStorage.getItem('myBookList')) || [];
 
-const collection = JSON.parse(localStorage.getItem('myBookList')) || [];
-
-function saveLocal() {
-  const acceptableString = JSON.stringify(collection);
-  localStorage.setItem('myBookList', acceptableString);
+class preserve {
+  static individualElems() {
+    for(let i = 0; i < collection.length; i++) {
+      displayList.create(collection[i]);
+    }
+  }
+  static initialBook() {
+    document.getElementById('sub').addEventListener('click', addBook);
+  }
 }
 
-function addItem(book) {
-  const mainList = document.getElementById('main-list');
-  const itemContainer = document.createElement('div');
-  mainList.appendChild(itemContainer);
-  const listItem = document.createElement('p');
-  const itemInfo = document.createTextNode(`Title: ${book.title} | Author: ${book.author}`);
-  listItem.appendChild(itemInfo);
-  itemContainer.appendChild(listItem);
-  const listBtn = document.createElement('button');
-  listBtn.innerText = 'Remove';
-  listBtn.addEventListener('click', (event) => {
-    const button = event.target;
-    button.parentNode.parentNode.removeChild(button.parentNode);
-    const itemIndex = collection.indexOf(book);
-    collection.splice(itemIndex, 1);
-    saveLocal();
-  });
-  itemContainer.appendChild(listBtn);
-  saveLocal();
-}
-
-function adBook(events) {
-    events.preventDefault();
-        const book = {
-        title: document.getElementById('title').value,
-        author: document.getElementById('author').value,
-    };
-    collection.push(book);
-    document.querySelector('form').reset();
-    addItem(book);
-}
-
+const pre = new preserve();
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('sub').addEventListener('click', adBook);
-    collection.forEach(addItem);
-});
-
-/* eslint-enable indent */
+  document.getElementById('sub').addEventListener('click', addBook);
+  preserve.initialBook();
+  preserve.individualElems();
+})
