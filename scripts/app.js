14423d36dfb134c7319dcd9ea3b01b84fa039a26
Luqman Musah <musahluqman@gmail.com>
2021-07-06 08:19:01 +0000

    Add perserve data functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 /* eslint-disable indent */
 
-const collection = [];
+let collection = JSON.parse(localStorage.getItem('myBookList'));
 
 function saveLocal() {
   const acceptableString = JSON.stringify(collection);
@@ -15,28 +15,32 @@ function adBook(events) {
     };
     collection.push(book);
     document.querySelector('form').reset();
-
-    const mainList = document.getElementById('main-list');
-    const itemContainer = document.createElement('div');
-    mainList.appendChild(itemContainer);
-    const listItem = document.createElement('p');
-    const itemInfo = document.createTextNode(`Title: ${book.title} | Author: ${book.author}`);
-    listItem.appendChild(itemInfo);
-    itemContainer.appendChild(listItem);
-    const listBtn = document.createElement('button');
-    listBtn.innerText = 'Remove';
-    listBtn.addEventListener('click', (event) => {
-      const button = event.target;
-      button.parentNode.parentNode.removeChild(button.parentNode);
-      const itemIndex = collection.indexOf(book);
-      collection.splice(itemIndex, 1);
-      saveLocal();
-    });
-    itemContainer.appendChild(listBtn);
-    saveLocal();
+    addItem(book);
+    
 }
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('sub').addEventListener('click', adBook);
+    collection.forEach(addItem);
 });
 
+function addItem(book) {
+  const mainList = document.getElementById('main-list');
+  const itemContainer = document.createElement('div');
+  mainList.appendChild(itemContainer);
+  const listItem = document.createElement('p');
+  const itemInfo = document.createTextNode(`Title: ${book.title} | Author: ${book.author}`);
+  listItem.appendChild(itemInfo);
+  itemContainer.appendChild(listItem);
+  const listBtn = document.createElement('button');
+  listBtn.innerText = 'Remove';
+  listBtn.addEventListener('click', (event) => {
+    const button = event.target;
+    button.parentNode.parentNode.removeChild(button.parentNode);
+    const itemIndex = collection.indexOf(book);
+    collection.splice(itemIndex, 1);
+    saveLocal();
+  });
+  itemContainer.appendChild(listBtn);
+  saveLocal();
+}
 /* eslint-enable indent */
